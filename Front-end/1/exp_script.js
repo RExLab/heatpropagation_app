f350b15e9a64ef4caa77714ab11e31279b6f571c
Lucas Júlio Silveira <lucaszaib@gmail.com>
2017-07-19 17:46:59 -0700

    update

--- Front-end/1/exp_script.js
+++ Front-end/1/exp_script.js
@@ -1,113 +1,86 @@
 $('head').append('<link rel="stylesheet" href="http://relle.ufsc.br/css/shepherd-theme-arrows.css" type="text/css"/>');
 
-var rpi_server = "http://conducaocalor1.relle.ufsc.br";
+var rpi_server = "http://propagacaocalor1.relle.ufsc.br";
 var t = new Date();
 var start = 0;
-var step = 2;
-var stephelp = 2;
+var step;
+var stephelp;
 var interval, time_plotting = 4 * 60;
-var timer = null; 
+var timer = null;
 var readings = [];
 var url;
-var $window = $(window);
-window.charts = [];
-var lab_socket= null;
 
-$.getScript('http://relle.ufsc.br/exp_data/13/1/welcome.js', function () {
+var lab_socket = null;
+
+$.getScript('http://relle.ufsc.br/exp_data/5/1/welcome.js', function () {
     var shepherd = setupShepherd();
     addShowmeButton('<button id="btnIntro" class="btn btn-sm btn-default"> <span class="long">' + lang.showme + '</span><span class="short">' + lang.showmeshort + '</span> <span class="how-icon fui-question-circle"></span></button>')
     $('#btnIntro').on('click', function (event) {
         event.preventDefault();
         shepherd.start();
     });
+
 });
 
+
 function done() {
-    console.log('<img> carregada com sucesso');
     url = document.getElementById("canvas").toDataURL();
     document.getElementById("imagem").value = url;
 }
 
-var randomScalingFactor = function () {
-    return Math.round(Math.random() * 100);
-};
-
-
-var lineChartData = {
-    labels: [],
-    datasets: [{
-            label: "Barra 1",
-            fillColor: "rgba(255, 211, 198, 0.1)",
-            strokeColor: "#FDA98F",
-            pointColor: "#FDA98F",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: []
-        }, {
-            label: "Barra 2",
-            fillColor: "rgba(151,187,205,0.1)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: []
-        }, {
-            label: "Barra 3",
-            fillColor: "rgba(130, 125, 143, 0.1)",
-            strokeColor: "rgba(130, 125, 143, 0.9)",
-            pointColor: "rgba(130, 125, 143, 0.9)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(130, 125, 143, 0.9)",
-            data: []
-
-
-
-        }]
-
+var chart = {
+    type: 'line',
+    data: {
+        datasets: [{
+                label: lang.label1,
+                data: [],
+                backgroundColor: "rgba(75,192,192,0.4)"
+            }, {
+                label: lang.label2,
+                data: [],
+                backgroundColor: "rgba(255, 211, 198, 0.1)"
+            }],
+        xLabels: [lang.time],
+        yLabels: [lang.temperature],
+        labels: [lang.temperature]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                    type: 'linear',
+                    position: 'bottom'
+                }]
+        }
+    }
 };
 
 function init_chart() {
-    if(document.getElementById("canvas") == null){
-        console.log('canvas element not found')
-        return;
-    }
+    console.log("Iniciando Grafico");
     var ctx = document.getElementById("canvas").getContext("2d");
-    var chart = {};
-    chart.lineChartData = lineChartData;
-    chart.index = [0, 3, 6];
-    chart.myLine = window.myLine = new Chart(ctx).Line(chart.lineChartData, {
-        responsive: true
-    });
-    $("#canvas").width('');
-    window.charts.push(chart);
+
+
+    if (typeof (window.myLine) != 'undefined')
+        window.myLine.destroy();
+
+    window.myLine = new Chart(ctx, chart);
 
 }
 
 function update_chart(thermos) {
-    for (var cIndex = 0; cIndex < window.charts.length; cIndex++) {
-        var data = [];
-        for (var i = 0; i < window.charts[cIndex].index.length; i++) {
-            data.push(thermos[window.charts[cIndex].index[i]]);
-        }
-        window.charts[cIndex].myLine.addData(data, start);
-    }
+    window.myLine.data.datasets[0].data.push({x: start, y: thermos[0]})
+    window.myLine.data.datasets[1].data.push({x: start, y: thermos[1]})
+    window.myLine.update();
     start = start + step;
-    $('.labelx').show();
-    $('.labely').show();
 }
 
-/*function update_chart(thermos) {
- window.myLine.addData([thermos[0], thermos[3], thermos[6]], start);
- start = start + step;
- }*/
-
 function ResizeSamples(stepold) {
-    lineChartData.labels = [];
+    window.myLine.data.datasets[0].data = [];
+    window.myLine.data.datasets[1].data = [];
+
     for (var i = 0; i < start; i = i + 2 * stepold) {
-        lineChartData.labels.push(i);
+        window.myLine.data.datasets[0].data.push({x: start, y: thermos[0]})
+        window.myLine.data.datasets[1].data.push({x: start, y: thermos[1]})
+
     }
 
     for (var j = 0; j < 3; j++) {
@@ -119,188 +92,96 @@ function ResizeSamples(stepold) {
                 index++;
             }
         }
-        console.log(" readings len: " + readings.length + " | new array len:" + newarray.length);
-        lineChartData.datasets[j].data = newarray;
-        console.log(lineChartData.datasets[j].data);
     }
-    return;
+    window.myLine.update();
 }
 
 $(function () {
-    $.getScript('http://relle.ufsc.br/js/Chart.js', function () {
+    $('[data-toggle="tooltip"]').tooltip();
 
-        $(".grafico").show();
-        $(".grafico.loading").hide();
+    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.2/Chart.min.js', function () {
+        init_chart();
     });
 
-
     $.getScript(rpi_server + '/socket.io/socket.io.js', function () {
-        var mult = 0;
+
+        //var mult = 0;
         lab_socket = io.connect(rpi_server);
-        //Conecta-se enviando chave de acesso ao lab
-        lab_socket.emit('new connection', {
-            pass: $('meta[name=csrf-token]').attr('content')
-        });
         
         lab_socket.on('reconnect', function () {
             lab_socket.emit('new connection', {pass: $('meta[name=csrf-token]').attr('content')} );
         });
-
-        $(".controllers").show();
-        $(".controllers.loading").hide();
-
-        // Send a message
         
+        $(".controllers").show();
+        $(".loading").hide();
 
+        $("#show-info span p").html(message[0]);
+        $("#show-info").show();
+        //$("#show-error span p").html(message[1]);
+        //$("#show-error").show();
+        //
+        //Conecta-se enviando chave de acesso ao lab
+        lab_socket.emit('new connection', {pass: $('meta[name=csrf-token]').attr('content')});
+
+        lab_socket.on('lab sync', function (data) {
+            if (data.iscooling) {
+                $("#show-error span p").html(message[1]);
+                $("#show-error").show();
+            }
+
+        });
         // Servidor deve mandar leituras a cada intervalo de tempo estabelecido
-        lab_socket.on('data received', function (data) {
-            var parsed = JSON.parse(data);
-            readings.push(parsed.thermometers);
-            if (mult == 0) {
-                update_chart(parsed.thermometers);
-            }
 
-            mult = (mult + stephelp) % step;
-            if (start > 0 && ((start % (stephelp * 16)) == 0) && step < 10) {
-                step = step * 2;
-                ResizeSamples(step / 2); //somente primeiros termometros
-                /*for (var cIndex = 0; cIndex < window.charts.length; cIndex++) {
-                 var data = [];
-                 for (var i = 0; i < window.charts[cIndex].index.length; i++) {
-                 
-                 }
-                 }*/
-                window.myLine.destroy();
-                var ctx = document.getElementById("canvas").getContext("2d");
-                window.myLine = new Chart(ctx).Line(lineChartData, {
-                    responsive: true
-                });
-            }
+
+        lab_socket.on('data received', function (data) {
+
+            $("#m1").show();
+            $("#m2").show();
+            var parsed = JSON.parse(data);
+            readings.push([parsed.thermometers[0], parsed.thermometers[1]]);
+            //console.log("nova mensagem"); 
+            //if (mult == 0) {
+            update_chart(parsed.thermometers);
+            /*var canvas = document.getElementById("canvas");
+             var ctx = canvas.getContext("2d");
+             ctx.font = "14px Arial";
+             ctx.fillText("T[°C]",0,14);
+             ctx.fillText("t[s]",canvas.width-20,canvas.height-10);*/
+            //}
+            //mult = (mult + stephelp) % step;
+            /*if (start > 0 && ((start % (stephelp * 16)) == 0) && step < 10) {
+             step = step * 2;
+             ResizeSamples(step / 2); //somente primeiros termometros
+             window.myLine.destroy();
+             var ctx = document.getElementById("canvas").getContext("2d");
+             window.myLine = new Chart(ctx).Line(lineChartData, {
+             responsive: true
+             });
+             }*/
         });
 
         $('#start').click(function () {
             $(this).prop('disabled', true);
-            $('#canvas').show();
-            init_chart();
-            $('.info-bars').hide();
             sendMessage(1);
         });
 
-        // TODO trocar por #btnLeaveExp
-        $('#btnLeaveExp').click(LabLeaveSessionHandler);
-       
+        // TODO #btnLeaveExp
+        $('#btnLeaveExp').on('click', LabLeaveSessionHandler);
 
-        $('#newvariable').on('click', function (event) {
-            event.preventDefault();
-            if ($('#matches div.row div').length < 17) {
-                addNewVariable();
-            }
-        });
-
-        $('#removevariable').on('click', function (event) {
-            event.preventDefault();
-            removeLastVariable();
-        });
-
-        $('#newchart').on('click', function (event) {
-            event.preventDefault();
-            var chart = addNewChart();
-            var ctx = $('canvas.chart').last()[0].getContext("2d");
-            chart.myLine = new Chart(ctx).Line(chart.lineChartData, {
-                responsive: true
-            });
-            chart.id = Math.floor(Math.random() * 1000);
-            $('canvas.chart').last().attr("id", chart.id);
-            $('canvas.chart').last().width('');
-            window.charts.push(chart);
-
-            removeAllVariables();
-
-            $('a.removechart').on('click', function (event) {
-                event.preventDefault();
-                removeChart($(this));
-            });
-        });
-
-        $('.settings').show();
     });
-
 });
 
-function removeChart(elEvent) {
-    var el = elEvent.parent('div.customchart');
-    var deletebyid = parseInt(el.find('canvas').attr('id'));
-    console.log(deletebyid);
+function report(id) {
 
-    for (var i = 0; i < window.charts.length; i++) {
-        if (window.charts[i].id == deletebyid) {
-            window.charts[i].myLine.destroy();
-            window.charts.splice(i, 1);
+    $.ajax({
+        type: "POST",
+        url: location.pathname + "/report/",
+        data: {dados: url},
+        success: function (pdf) {
+            var win = window.open(location.pathname + "/report", '_blank');
+            console.log("Report created.");
         }
-    }
-    el.remove();
-
-}
-
-function addNewVariable() {
-    $('#matches div.row').append("<div class='versus col-lg-1 col-md-1 col-sm-1 col-xs-12'>x</div>" + "<div class='select-group col-lg-3 col-md-5 col-sm-5 col-xs-12'> " + "<select class='form-control'>" + "<option></option>" + "<option>M1-T1</option>" + "<option>M1-T2</option>" + "<option>M1-T3</option>" + "<option>M2-T1</option>" + "<option>M2-T2</option>" + "<option>M2-T3</option>" + "<option>M3-T1</option>" + "<option>M3-T2</option>" + "<option>M3-T3</option>" + "</select>" + "</div>");
-}
-
-function removeLastVariable() {
-    if ($('#matches div.row div').length > 1) {
-        $('#matches div.row div.select-group').last().remove();
-        $('#matches div.row div.versus').last().remove();
-    }
-}
-
-function removeAllVariables() {
-    while ($('#matches div.row div').length > 1) {
-        removeLastVariable();
-    }
-}
-
-function addNewChart() {
-    var chart = {};
-
-    $("#custom-group").append('<div class="col-lg-6 col-sm-12 col-md-6 col-xs-12 customchart" >' + '<a href="#" class="removechart"><span class="glyphicon glyphicon-trash"></span></a>' + '<div class="labely">' + lang.temperature + ' [&#8451;]</div>' + '<canvas class="col-lg-12 chart" height="320" width="480">Aguarde, o gráfico está sendo configurado...</canvas>' + '<div class="labelx">' + lang.time + ' [s]</div>' + '</div>');
-
-    var thisDiv = $("#custom-group div.customchart").last();
-    thisDiv.append('<div class="row"></div>');
-    chart.index = [];
-
-    chart.lineChartData = {};
-    chart.lineChartData.labels = [];
-    for (var i = 0; i < start; i += stephelp) {
-        chart.lineChartData.labels.push(i);
-    }
-
-    chart.lineChartData.datasets = [];
-
-    $('#matches div.row div.select-group').each(function (index, element) {
-        var variablename = $(element).find('select').val().split('-');
-        if (variablename.length > 1) {
-            thisDiv.find('div.row').append('<div class="col-sm-4 col-xs-4"> <canvas class="legend" style="background:' + lang.strokes[variablename[0] + '-' + variablename[1]] + ';"></canvas> ' + variablename[1] + ' ' + lang.metals[variablename[0]] + '</div>');
-            var index = parseInt(variablename[1][1]) + 3 * (parseInt(variablename[0][1]) - 1) - 1;
-            chart.index.push(index);
-            var datatemp = [];
-            for (var i = 0; i < readings.length; i++) {
-                datatemp.push(readings[i][index]);
-            }
-            chart.lineChartData.datasets.push({
-                label: "Barra 1",
-                fillColor: "rgba(255, 211, 198, 0.1)",
-                strokeColor: lang.strokes[variablename[0] + '-' + variablename[1]],
-                pointColor: lang.strokes[variablename[0] + '-' + variablename[1]],
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: datatemp
-            });
-        }
-
     });
-    console.log(chart);
-    return chart;
 }
 
 function LabLeaveSessionHandler() {
@@ -312,24 +193,23 @@ function LabLeaveSessionHandler() {
         lab_socket.disconnect();
         lab_socket = null;
     }
-
 }
 
-function report(id) {
-  
-    console.log('<img> converted');
-    console.log(url);
-    $.ajax({
-        type: "POST",
-        url: location.pathname + "/report/",
-        data: {
-            dados: url
-        },
-        success: function (pdf) {
-            var win = window.open(location.pathname + "/report", '_blank');
-            console.log("Report created.");
-        }
-    });
+function sendMessage(status) { 
+    var message = {};
+    message.sw = status;
+    step = message.step = parseInt($("#step").val());
+    message.duration = parseInt($("#sourceduration").val());
+    message.power = $('input:radio[name=power]:checked').val();
+    stephelp = step;
+    console.log(message);
+    if (message) {
+        var time = new Date();
+        message.date = time;
+        message.pass = $('meta[name=csrf-token]').attr('content');
+        lab_socket.emit('start', message);
+        start = 0;
+    }
 }
 
 function exportcsv() {
@@ -339,31 +219,11 @@ function exportcsv() {
     for (var i = 0; i < readings.length; i++) {
         var dataline = {};
         dataline["time"] = i * stephelp;
-        for (var j = 0; j < 9; j++) {
+        for (var j = 0; j < 2; j++) {
             dataline["Thermo" + (j + 1)] = readings[i][j];
         }
         data[i.toString()] = dataline;
     }
-    $.redirect('http://relle.ufsc.br/labs/export/', data);
+    $.redirect(location.pathname + '/export/', data);
 
 }
-
-function sendMessage(status) {
-            var message = {};
-            message.sw = status;
-            step = message.step = parseInt($("#step").val());
-            message.duration = 240;
-            stephelp = step;
-            console.log(message);
-            if (message) {
-                var time = new Date();
-                message.date = time;
-                message.pass = $('meta[name=csrf-token]').attr('content');
-                lab_socket.emit('start', message);
-                start = 0;
-            }
-            
-            $('.labelx').show();
-            $('.labely').show();
-
-        }
--- Front-end/1/welcome.js
+++ Front-end/1/welcome.js
@@ -9,7 +9,7 @@ var setupShepherd = function () {
     });  
     // Entrar no tour 
     shepherd.addStep('shep_intro', { 
-        text: [lang.comeco],
+        text: [lang.intro1],
         classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
         buttons: [
             {
@@ -28,31 +28,10 @@ var setupShepherd = function () {
         ]
     });
     
-    // Entrar no tour 
-    shepherd.addStep('shep_intro', { 
-        text: [lang.intro1],
-        attachTo:'.cam right',
-        classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
-        buttons: [
-            {
-                text: lang.previous,
-                classes: 'shepherd-button-secondary',
-                action: function () {
-                    shepherd.back();
-                }
-            }, {
-                text: lang.next,
-                action: function () {
-                    shepherd.next();
-                },
-                classes: 'shepherd-button-example-primary'
-            }
-        ]
-    });
     // Tour no meio
-    shepherd.addStep('shep_grafico', { 
-        text: [lang.intro2],
-        attachTo: 'div.row.grafico center',
+    shepherd.addStep('shep_cam', { 
+        text: [lang.introcamera],
+        attachTo: 'img.cam center',
         classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
         buttons: [
             {
@@ -74,9 +53,8 @@ var setupShepherd = function () {
     
     
   
-    shepherd.addStep('shep_legenda', { 
-        text: [lang.intro3],
-        attachTo: '.legendas left',
+    shepherd.addStep('shep_intro2', { 
+        text: [lang.intro2],
         classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
         buttons: [
             {
@@ -95,50 +73,9 @@ var setupShepherd = function () {
         ]
     });
     
-    
-    shepherd.addStep('shep_barras', { 
-        text: [lang.intro4],
-        attachTo:".barras",
-        classes: "shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text",
-        buttons: [
-            {
-                text: lang.previous,
-                classes: 'shepherd-button-secondary',
-                action: function () {
-                    shepherd.back();
-                }
-            }, {
-                text: lang.next,
-                action: function () {
-                    shepherd.next();
-                },
-                classes: 'shepherd-button-example-primary'
-            }
-        ]
-    });
-    shepherd.addStep('shep_termo', { 
-        text: [lang.intro5],
-        attachTo: '#matches bottom',
-        classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
-        buttons: [
-            {
-                text: lang.previous,
-                classes: 'shepherd-button-secondary',
-                action: function () {
-                    shepherd.back();
-                }
-            }, {
-                text: lang.next,
-                action: function () {
-                    shepherd.next();
-                },
-                classes: 'shepherd-button-example-primary'
-            }
-        ]
-    });
-    shepherd.addStep('shep_curvas', { 
-        text: [lang.intro6],
-        attachTo: '#newvariable botton',
+    shepherd.addStep('shep_time', { 
+        text: [lang.introtime],
+        attachTo: 'switches',
         classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
         buttons: [
             {
@@ -157,9 +94,9 @@ var setupShepherd = function () {
         ]
     });
     
-    shepherd.addStep("shep.erro", { 
-        text: [lang.intro7],
-        attachTo: '#removevariable bottom',
+    shepherd.addStep('shep_power', { 
+        text: [lang.power],
+        attachTo: 'form.power top',
         classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
         buttons: [
             {
@@ -177,9 +114,9 @@ var setupShepherd = function () {
             }
         ]
     });
-    shepherd.addStep('shep_novo', {
-        text: [lang.intro8],
-        attachTo: "#newchart bottom",
+    shepherd.addStep('shep_istart', { 
+        text: [lang.start],
+        attachTo: 'input#start center',
         classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
         buttons: [
             {
@@ -197,9 +134,9 @@ var setupShepherd = function () {
             }
         ]
     });
-    shepherd.addStep('shep_intervalo', { 
-        text: [lang.intro9],
-        attachTo: '#step bottom',
+    shepherd.addStep('shep_canvas', { 
+        text: [lang.chart],
+        attachTo: '#canvas center',
         classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
         buttons: [
             {
@@ -217,9 +154,8 @@ var setupShepherd = function () {
             }
         ]
     });
-    shepherd.addStep('shep_fonte', { 
-        text: [lang.intro10],
-        attachTo: '#start bottom',
+    shepherd.addStep('shep_report', { 
+        text: [lang.report],
         classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
         buttons: [
             {
@@ -237,11 +173,12 @@ var setupShepherd = function () {
             }
         ]
     });
+    
     // Finaliza Tour
     
-    shepherd.addStep('shep_volte', { 
-        text: [lang.intro11],
-        attachTo: ".volte bottom",
+    shepherd.addStep('shep_alerts', { 
+        text: [lang.alerts],
+        attachTo: "div.alerts center",
         classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
         buttons: [
             {
@@ -249,10 +186,10 @@ var setupShepherd = function () {
                 classes: 'shepherd-button-secondary',
                 action: function () {
                     shepherd.back();
-                }  
-            }, { 
+                }
+            }, {
                 text: lang.done,
-                action: function () {   
+                action: function () {
                     shepherd.cancel();
                 },
                 classes: 'shepherd-button-example-primary'
